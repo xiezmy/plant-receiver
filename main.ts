81fe04ddbc314e15c07a98397c71ba490106e1d4
xiezmy <briannachea2008@icloud.com>
2024-07-24 11:34:37 -0700

    receiver for auto alert, B button now for stopping repeat alert

--- main.ts
+++ main.ts
@@ -2,7 +2,8 @@ enum RadioMessage {
     message1 = 49434,
     check_plant_wetness = 9373,
     happy = 4585,
-    sad = 2621
+    sad = 2621,
+    need_water = 18906
 }
 input.onButtonPressed(Button.A, function () {
     radio.sendMessage(RadioMessage.check_plant_wetness)
@@ -16,5 +17,20 @@ radio.onReceivedMessage(RadioMessage.sad, function () {
     music.setVolume(127)
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wawawawaa), music.PlaybackMode.InBackground)
     basic.showIcon(IconNames.Sad)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    music.stopMelody(MelodyStopOptions.All)
+})
+radio.onReceivedMessage(RadioMessage.need_water, function () {
+    music.setVolume(255)
+    music.play(music.stringPlayable("C5 - C5 - C5 - C5 - ", 190), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # . # . #
+        . . . . .
+        # . # . #
+        `)
 })
 radio.setGroup(91407)
